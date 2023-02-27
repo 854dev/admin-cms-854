4778e4f6c7bb088f0711bb78c4c155a4c1703f99
854 <=>
2023-02-27 17:36:59 +0900

    edit ContentAdd : 중간저장

--- src/views/ContentAdd.tsx
+++ src/views/ContentAdd.tsx
@@ -3,7 +3,6 @@ import Footer from 'layouts/partials/Footer';
 import Badge from 'components/Badge';
 import Breadcrumb, { BreadcrumbItem } from 'components/Breadcrumb';
 import Button from 'components/Button';
-import CustomSelect from 'components/form/CustomSelect';
 import Input from 'components/form/Input';
 import Label from 'components/form/Label';
 import Textarea from 'components/form/Textarea';
@@ -12,13 +11,37 @@ import FormMeta from './content/FormMeta';
 import api from 'api';
 import TableComponent from 'components/Table';
 import { useParams } from 'react-router-dom';
-import { ContentBody, ContentMeta } from 'types/common';
-import { useLayoutEffect, useRef, useState } from 'react';
+import { ContentBody, ContentMeta, ContentType } from 'types/common';
+import {
+  ChangeEvent,
+  ChangeEventHandler,
+  useEffect,
+  useLayoutEffect,
+  useRef,
+  useState,
+} from 'react';
 import FormBody from './content/FormBody';
 import Dropdown from 'components/Dropdown';
+import CustomSelect from 'components/form/CustomSelect';
 
-const Content = () => {
-  const { contentId } = useParams();
+const ContentAdd = () => {
+  const contentBodyRef = useRef<{ contentBody: ContentBody[] }>({ contentBody: [] });
+
+  const [contentType, setcontentType] = useState('-1');
+
+  const [contentBody, setContentBody] = useState<ContentBody[]>([]);
+
+  const {
+    data: contentTypeListData,
+    error: contentTypeListError,
+    isSuccess: contentTypeListSuccess,
+  } = api.useGetContentTypeListQuery({
+    page: 1,
+    limit: 50,
+  });
+
+  const [contentTypeDetailTrigger, contentTypeDetailResult] =
+    api.useLazyGetContentTypeDetailQuery();
 
   const [contentMeta, setContentMeta] = useState<ContentMeta>({
     title: '',
@@ -29,37 +52,30 @@ const Content = () => {
     status: 'draft',
   });
 
-  const [contentBody, setContentBody] = useState<ContentBody[]>([]);
-
-  const contentBodyRef = useRef<{ contentBody: ContentBody[] }>({ contentBody: [] });
-
-  const [fetchContentDetail, contentDetailResponse] = api.useLazyGetContentDetailQuery();
-
-  const [triggerPatchContent, patchContentResponse] = api.usePutContentMutation();
-
-  const getContentDetail = async (contentId: number) => {
-    const res = await fetchContentDetail(contentId).unwrap();
-    setContentMeta({ ...res, body: undefined });
-    setContentBody(res.body);
-    contentBodyRef.current.contentBody = res.body;
+  const onChangeContentType: ChangeEventHandler<HTMLSelectElement> = async (e) => {
+    getContentTypeDetail(e.currentTarget.value);
   };
 
-  // TODO
-  const onSubmit = async () => {
-    const param = {
-      contentId: contentId ?? '',
-      ...contentMeta,
-      body: contentBodyRef.current.contentBody,
-    };
-    setContentBody(contentBodyRef.current.contentBody);
-    const res = await triggerPatchContent(param);
+  const getContentTypeDetail = async (id: string) => {
+    const res = await contentTypeDetailTrigger(id).unwrap();
+    console.log(res);
+    // contentBodyRef.current.contentBody = res;
+    console.log(contentBodyRef.current.contentBody);
   };
 
-  useLayoutEffect(() => {
-    if (contentId) {
-      getContentDetail(Number(contentId));
+  const onSubmit = () => {
+    console.log('first');
+  };
+
+  useEffect(() => {
+    if (contentTypeListSuccess) {
+      if (contentTypeListData.data.length > 1) {
+        const firstId = contentTypeListData.data[0].id;
+        setcontentType(firstId);
+        getContentTypeDetail(firstId);
+      }
     }
-  }, [contentId]);
+  }, [contentTypeListSuccess]);
 
   return (
     <main className='workspace'>
@@ -69,9 +85,24 @@ const Content = () => {
           <Breadcrumb title={'Content'}>
             <BreadcrumbItem>Content</BreadcrumbItem>
             <BreadcrumbItem>Add</BreadcrumbItem>
-            {contentId ? <BreadcrumbItem>{contentId}</BreadcrumbItem> : null}
           </Breadcrumb>
         </section>
+
+        {/* content type Select */}
+        <div className='card relative mb-5 p-4'>
+          <h3>콘텐츠 타입 선택</h3>
+          <CustomSelect onChange={onChangeContentType}>
+            {contentTypeListData ? (
+              <>
+                {contentTypeListData.data.map((elem: ContentType) => (
+                  <option key={elem.id} value={elem.id}>
+                    {elem.name}
+                  </option>
+                ))}
+              </>
+            ) : null}
+          </CustomSelect>
+        </div>
 
         <div className='card mb-5 p-4'>
           <h3 className='mb-4'>콘텐츠 정보</h3>
@@ -80,10 +111,13 @@ const Content = () => {
 
         <div className='card p-4'>
           <h3 className='mb-4'>콘텐츠 내용</h3>
-          <FormBody
+          {JSON.stringify(
+            contentTypeDetailResult.isSuccess ? contentTypeDetailResult.data.bodyField : 'ㅗ'
+          )}
+          {/* <FormBody
             ref={contentBodyRef}
-            contentBody={contentBody ?? contentBodyRef.current.contentBody}
-          ></FormBody>
+            contentBody={contentBodyRef.current.contentBody}
+          ></FormBody> */}
         </div>
 
         <div className='flex flex-row justify-end p-4'>
@@ -95,4 +129,4 @@ const Content = () => {
   );
 };
 
-export default Content;
+export default ContentAdd;
