7c23b909b6b503e04bc65125fdccda757e16b50a
854 <=>
2023-02-27 18:31:17 +0900

    add createContentBodyFromBodyField

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,1 +1,11 @@
-export {};
+import { ContentBodyField } from 'types/common';
+
+export const createContentBodyFromBodyField = (bodyFields: ContentBodyField[]) => {
+  return bodyFields.map((elem) => {
+    return {
+      bodyFieldId: elem.fieldTypeId,
+      bodyFieldName: elem.fieldName,
+      bodyFieldValue: '',
+    };
+  });
+};
