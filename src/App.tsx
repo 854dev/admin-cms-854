927f78aacca18978b7c4a57d21b43696660eed4e
854 <=>
2023-02-28 16:09:16 +0900

    add AlertContainer

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,13 @@ import './App.css';
 
 import Router from './routes';
 import Store from './features/store';
+import AlertContainer from 'layouts/partials/AlertContainer';
 
 const App = () => {
   return (
     <Provider store={Store}>
       <Router />
+      <AlertContainer />
     </Provider>
   );
 };
