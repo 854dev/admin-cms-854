32b05d84d74b11c334ead9a2e4976d7c62e31d72
854 <=>
2023-03-15 19:11:54 +0900

    edit ContentEnum

--- src/views/ContentEnum.tsx
+++ src/views/ContentEnum.tsx
@@ -1,4 +1,5 @@
 import Breadcrumb, { BreadcrumbItem } from 'components/Breadcrumb';
+import Button from 'components/Button';
 import React, { useState } from 'react';
 
 function ContentEnum() {
@@ -15,11 +16,40 @@ function ContentEnum() {
           </Breadcrumb>
         </section>
 
-        <div className='card'>
-          <ul>
-            <li>1</li>
-            <li>2</li>
-          </ul>
+        <div className='w-full overflow-y-scroll'>
+          <div className='card min-h-[20rem] p-2'>
+            <div className='grid grid-flow-col gap-4'>
+              <table className='grid-col-4 table_hoverable table w-full table-auto'>
+                <thead>
+                  <th>이름</th>
+                </thead>
+                <tbody>
+                  <tr className={currntEnumName === 'reaction' ? 'bg-gray-100 text-primary' : ''}>
+                    <td>게시물반응</td>
+                  </tr>
+                  <tr className={currntEnumName === 'chinafood' ? 'bg-gray-100 text-primary' : ''}>
+                    <td>중식메뉴</td>
+                  </tr>
+                </tbody>
+              </table>
+
+              <div className='grid-col-1 border-l-2 p-1'></div>
+
+              <table className='grid-col-4 table_hoverable table w-full table-auto'>
+                <thead>
+                  <th>값 목록</th>
+                </thead>
+                <tbody>
+                  <tr className={currntEnumName === 'reaction' ? 'bg-gray-100 text-primary' : ''}>
+                    <td>게시물반응</td>
+                  </tr>
+                  <tr className={currntEnumName === 'chinafood' ? 'bg-gray-100 text-primary' : ''}>
+                    <td>중식메뉴</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
       </div>
     </main>
