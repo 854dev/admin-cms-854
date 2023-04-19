de175dcc728e9c56bd0be1f3abbf8561969a99a6
854 <jahae854@gmail.com>
2023-04-20 00:24:51 +0900

    edit footer

--- src/layouts/partials/Footer.tsx
+++ src/layouts/partials/Footer.tsx
@@ -2,14 +2,14 @@ const Footer = () => {
   return (
     <footer className='mt-auto'>
       <div className='footer'>
-        <span className='uppercase'>&copy; 2022 Yeti Themes</span>
-        <nav>
+        <span className='uppercase'>&copy; LocalLink | created using 2022 Yeti Themes</span>
+        {/* <nav>
           <a href='mailto:Yeti Themes<info@yetithemes.net>?subject=Support'>Support</a>
           <span className='divider'>|</span>
           <a href='http://yeti.yetithemes.net/docs' target='_blank' rel='noreferrer'>
             Docs
           </a>
-        </nav>
+        </nav> */}
       </div>
     </footer>
   );
