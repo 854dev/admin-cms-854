22ec6682cd92e79bfb77760c0010e891f2ff33f2
jahae854 <jahae854@gmail.com>
2023-04-20 21:36:28 +0900

    add apiThenShowMessage

--- src/util/util.ts
+++ src/util/util.ts
@@ -1,19 +1,32 @@
-import { ContentBody, ContentBodySchema, ContentBodyWithName } from 'types/common';
 import { DateTime } from 'luxon';
 import { DATE_FORMAT } from './constant';
-
-export const createContentBodyFromBodyField: (
-  bodyFields: ContentBodySchema[]
-) => ContentBodyWithName[] = (bodyFields) => {
-  return bodyFields.map((elem) => {
-    return {
-      schemaId: elem.schemaId,
-      schemaName: elem.schemaName,
-      schemaValue: '',
-    };
-  });
-};
+import store from 'features/store';
+import { alertSlice } from 'features/alertSlice';
 
 export const parseDate = (dateString: string, format?: string) => {
   return DateTime.fromISO(dateString).toFormat(format ?? DATE_FORMAT);
 };
+
+export function apiThenShowMessage(promise: any) {
+  promise
+    .then((response: any) => {
+      store.dispatch(
+        alertSlice.actions.setAlert({
+          title: `성공 : ${response.message}`,
+          color: 'success',
+          dismissable: true,
+          outlined: false,
+        })
+      );
+    })
+    .catch((error: any) => {
+      store.dispatch(
+        alertSlice.actions.setAlert({
+          title: `${JSON.stringify(error.data?.message)}`,
+          color: 'danger',
+          dismissable: true,
+          outlined: false,
+        })
+      );
+    });
+}
