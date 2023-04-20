8a342f8bb3521903c8cc925d9e4af596188bbb14
jahae854 <jahae854@gmail.com>
2023-04-20 19:26:36 +0900

    add TopBar

--- src/layouts/Default.tsx
+++ src/layouts/Default.tsx
@@ -1,13 +1,14 @@
 import { Outlet } from 'react-router-dom';
 
-import Navigation from 'layouts/partials/Navigation';
 import Footer from './partials/Footer';
+import TopBar from './partials/TopBar';
 
 const LayoutDefault = () => {
   return (
     <>
-      <div className='w-full'>
-        <Navigation />
+      <TopBar />
+
+      <div className='w-full pt-16'>
         <Outlet />
       </div>
     </>
---
+++ src/layouts/partials/TopBar.tsx
@@ -0,0 +1,90 @@
+import Dropdown from 'components/Dropdown';
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const TopBar = () => {
+  const [menuBarVisible, setMenuBarVisible] = useState(true);
+
+  const toggleMenu = () => {
+    setMenuBarVisible(!menuBarVisible);
+  };
+
+  return (
+    <header className='top-bar'>
+      {/* Menu Toggler */}
+      <Dropdown
+        arrow={true}
+        content={
+          <div className='w-64'>
+            <div className='p-5'>
+              <NavLinks />
+            </div>
+
+            <div className='p-5'>
+              <h5 className='uppercase'>John Doe</h5>
+              <p>Editor</p>
+            </div>
+            <hr />
+            <div className='p-5'>
+              <a
+                href='#no-link'
+                className='flex items-center text-gray-700 hover:text-primary dark:text-gray-500 dark:hover:text-primary'
+              >
+                <span className='la la-user-circle text-2xl leading-none ltr:mr-2 rtl:ml-2'></span>
+                View Profile
+              </a>
+              <a
+                href='#no-link'
+                className='mt-5 flex items-center text-gray-700 hover:text-primary dark:text-gray-500 dark:hover:text-primary'
+              >
+                <span className='la la-key text-2xl leading-none ltr:mr-2 rtl:ml-2'></span>
+                Change Password
+              </a>
+            </div>
+            <hr />
+            <div className='p-5'>
+              <a
+                href='#no-link'
+                className='flex items-center text-gray-700 hover:text-primary dark:text-gray-500 dark:hover:text-primary'
+              >
+                <span className='la la-power-off text-2xl leading-none ltr:mr-2 rtl:ml-2'></span>
+                Logout
+              </a>
+            </div>
+          </div>
+        }
+      >
+        <button className='ltr:ml-4 rtl:mr-4'>
+          <button className='menu-toggler la la-bars' onClick={() => toggleMenu()}></button>
+        </button>
+      </Dropdown>
+
+      {/* Brand */}
+      <span className='brand'>Localink Admin</span>
+    </header>
+  );
+};
+
+export function NavLinks() {
+  const LinkItem = ({ to, name }: { to: string; name: string }) => {
+    return (
+      <li className='rounded-sm'>
+        <Link to={to}>
+          <div className='flex items-center space-x-3 rounded-md p-2'>
+            <span>{name}</span>
+          </div>
+        </Link>
+      </li>
+    );
+  };
+
+  return (
+    <ul>
+      <LinkItem to={'/'} name={'Dashboard'} />
+      <LinkItem to={'/content'} name={'Content'} />
+      <LinkItem to={'/content-type'} name={'Content Type'} />
+    </ul>
+  );
+}
+
+export default TopBar;
