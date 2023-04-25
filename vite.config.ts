c66c154acc4cdbea2ee1917830ddb2e31d9d6a0d
jahae854 <jahae854@gmail.com>
2023-04-25 14:53:15 +0900

    edit config : assetsDir

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 export default defineConfig({
   build: {
     sourcemap: true,
-    assetsDir: 'admin/',
+    assetsDir: 'admin/assets/',
   },
   plugins: [tsconfigPaths(), react()],
   server: {
