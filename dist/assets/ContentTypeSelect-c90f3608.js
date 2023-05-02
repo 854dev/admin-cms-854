88094603351f73e371584e488db6715254534311
854 <jahae854@gmail.com>
2023-05-03 00:48:50 +0900

    build 230503

---
+++ dist/assets/ContentTypeSelect-c90f3608.js
@@ -0,0 +1,5 @@
+import{j as e}from"./index-af880b26.js";const i=({contentTypeId:n,setcontentTypeId:o,contentTypeList:r})=>{const s=t=>{o(t)};return e.jsx("div",{children:r.map(t=>e.jsx("button",{className:`
+            m-1
+            ${t.contentTypeId===n?"bg-primary text-white":""}
+            `,onClick:()=>s(t.contentTypeId),children:t.contentTypeName},t.contentTypeId))})};export{i as C};
+//# sourceMappingURL=ContentTypeSelect-c90f3608.js.map
--- dist/assets/Dashboard-5c04d229.js
+++
@@ -1,2 +0,0 @@
-import{j as r}from"./index-4cc60571.js";function o(){return r.jsx("div",{children:"Dashboard"})}export{o as default};
-//# sourceMappingURL=Dashboard-5c04d229.js.map
