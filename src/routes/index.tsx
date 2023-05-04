e31f9ec2f9ebb0dd2983edec4ba370d01ac7eb2b
854 <jahae854@gmail.com>
2023-05-05 03:01:05 +0900

    edit needAuth

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,6 +3,8 @@ import { lazy, Suspense } from "react";
 import LayoutDefault from "layouts/Default";
 
 import Login from "views/Login";
+import { useSelector } from "react-redux";
+import { RootState } from "features/store";
 
 const Dashboard = lazy(() => import("views/Dashboard"));
 const ContentType = lazy(() => import("views/ContentType"));
@@ -16,6 +18,7 @@ export const route = {
     element: <Login />,
     absPath: "/admin",
     isNav: true,
+    needAuth: false,
   },
 
   dashboard: {
@@ -23,39 +26,52 @@ export const route = {
     element: <Dashboard />,
     absPath: "/admin/dashboard",
     isNav: true,
+    needAuth: true,
   },
   content: {
     path: "content",
     element: <Content />,
     absPath: "/admin/content",
     isNav: true,
+    needAuth: true,
   },
   contentAdd: {
     path: "content/add",
     element: <ContentAdd />,
     absPath: "/admin/content/add",
     isNav: true,
+    needAuth: true,
   },
   contentDetail: {
     path: "content/:contentId",
     element: <ContentDetail />,
     absPath: "/admin/content/:contentId",
     isNav: false,
+    needAuth: true,
   },
   contentType: {
     path: "content-type",
     element: <ContentType />,
     absPath: "/admin/content-type",
     isNav: true,
+    needAuth: true,
   },
 };
 
 const Router = () => {
+  const account = useSelector((state: RootState) => {
+    return state.account;
+  });
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="admin" element={<LayoutDefault />}>
           {Object.values(route).map((elem) => {
+            if (elem.needAuth && account.token === null) {
+              return null;
+            }
+
             return (
               <Route
                 key={elem.path}
@@ -68,6 +84,7 @@ const Router = () => {
               />
             );
           })}
+          <Route path={"*"} element={<div>404 NOT FOUND</div>} />
         </Route>
       </Routes>
     </BrowserRouter>
