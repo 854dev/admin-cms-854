367cb1f05d62273d42d8d651a15c77a416a165f3
854 <jahae854@gmail.com>
2023-05-05 03:02:20 +0900

    build 230505

--- dist/assets/ContentTypeSelect-306bef0e.js
+++
@@ -1,4 +0,0 @@
-import{j as n}from"./index-e03c4bb4.js";const a=({contentTypeId:r,setcontentTypeId:i,contentTypeList:o,disabled:s,onChange:e})=>{const c=t=>{i(t.contentTypeId),e&&e(t)};return n.jsx("div",{children:o.map(t=>n.jsx("button",{disabled:s,className:`
-            m-1
-            ${t.contentTypeId===r?"bg-primary text-white":""}
-            `,onClick:()=>c(t),children:t.contentTypeName},t.contentTypeId))})};export{a as C};
--- dist/assets/Dashboard-b16f9811.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-e03c4bb4.js";function o(){return r.jsx("div",{children:"Dashboard"})}export{o as default};
